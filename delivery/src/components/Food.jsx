a3667e195f15f49816017d7c5a484690b25bf794
Agora Ngelale <agorangelale@Agoras-MacBook-Air-3.local>
2023-12-25 12:50:29 -0800

    finishing touches

--- delivery/src/components/Food.jsx
+++ delivery/src/components/Food.jsx
@@ -2,22 +2,47 @@ import React, {useState}from 'react'
 import {data} from '../Data/Data.js'
 
 const Food = () => {
+  // Filter food type
+  const filterType = (category) =>{
+    setFoods(
+      data.filter((item)=>{
+        return item.category === category;
+      })
+    )
+  }
+  // Filter food type
+  const filterPrice = (price) =>{
+    setFoods(
+      data.filter((item)=>{
+        return item.price === price;
+      })
+    )
+  }
+const [filterAll, setFilterAll] = useState(false)
+const [filterBurger, setFilterBurger] = useState(false)
+const [filterPizza, setFilterPizza] = useState(false)
+const [filterSalad, setFilterSalad] = useState(false)
+const [filterChicken, setFilterChicken] = useState(false)
 const [foods, setFoods] = useState(data)
   return (
-    <div className='max-w-[1640px] m-auto px-4 py12'>
-      <h1 className='text-orange-600 font-bold text-center'>Top Rated Menu Items</h1>
-      
+    <div className='max-w-[1640px] m-auto px-4 py-12'>
+      <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
       {/* Filter Row */}
       <div className='flex flex-col lg:flex-row justify-between'>
         {/* Filter Type */}
         <div>
             <p className='font-bold text-gray-700'>Filter Type</p>
             <div className='flex justify-between flex-wrap'>
-              <button className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '>All</button>
-              <button className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600'>Burgers</button>
-              <button className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600'>Pizza</button>
-              <button className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600'>Salad</button>
-              <button className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600'>Chicken</button>
+              <button onClick={()=>{setFoods(data); setFilterAll(!filterAll)}}
+              className={filterAll? 'm-1 border-orange-600 text-white bg-orange-600': 'm-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '}>All</button>
+              <button onClick={()=>{filterType('burger'); setFilterBurger(!filterBurger)}}  
+              className={filterBurger? 'm-1 border-orange-600 text-white bg-orange-600': 'm-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '}>Burgers</button>
+              <button onClick={()=>{filterType('pizza'); setFilterPizza(!filterPizza)}} 
+               className={filterPizza? 'm-1 border-orange-600 text-white bg-orange-600': 'm-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '}>Pizza</button>
+              <button onClick={()=>{filterType('salad'); setFilterSalad(!filterSalad)}} 
+               className={filterSalad? 'm-1 border-orange-600 text-white bg-orange-600': 'm-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '}>Salad</button>
+              <button onClick={()=>{filterType('chicken'); setFilterChicken(!filterChicken)}} 
+               className={filterChicken? 'm-1 border-orange-600 text-white bg-orange-600': 'm-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '}>Chicken</button>
             </div>
         </div>
 
@@ -25,22 +50,27 @@ const [foods, setFoods] = useState(data)
         <div>
             <p className='font-bold text-gray-700'>Filter Price</p>
             <div className='flex justify-between max-w-[390px] w-full'>
-                <button className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '>$</button>
-                <button className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '>$$</button>
-                <button className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '>$$$</button>
-                <button className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '>$$$$</button>
+                <button onClick={()=>filterPrice('$')}
+                className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '
+                >$</button>
+                <button onClick={()=>filterPrice('$$')} className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '>$$</button>
+                <button onClick={()=>filterPrice('$$$')} className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '>$$$</button>
+                <button onClick={()=>filterPrice('$$$$')} className='m-1 border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 '>$$$$</button>
             </div>
         </div>
       </div>
       {/* Display foods */}
-      <div>
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+        {/* Food Cards */}
        {foods.map((item,index)=>(
-        <div key={index}>
-            <img src={item.image} alt={item.name}/>
-            <div>
-                <p>{item.name}</p>
+        <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
+            <img src={item.image} alt={item.name} 
+            className='w-full h-[200px] object-cover rounded-t-lg'
+            />
+            <div className='flex justify-between px-2 py-4'>
+                <p className='font-bold'>{item.name}</p>
                 <p>
-                    <span>{item.price}</span>
+                    <span className='bg-orange-500 text-white p-1 rounded-full'>{item.price}</span>
                 </p>
             </div>
         </div>
--- delivery/src/components/HeadlineCards.jsx
+++ delivery/src/components/HeadlineCards.jsx
@@ -9,7 +9,13 @@ const HeadlineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white '>
             <p className='font-bold text-2xl px-2 pt-4'>Sun's out, BOGO's Out</p>
             <p className='px-2'>Through 9/26</p>
-            <button className='border-white bg-white text-black absolute mx-2 bottom-4'>Order Now</button>
+            <button 
+            className='border-white bg-white text-black 
+            absolute mx-2 bottom-4 hover:text-white hover:bg-orange-600 
+            hover:scale-105 duration-300'
+            >
+              Order Now
+            </button>
         </div>
         <img className='max-h[160px] md:max-h-[200px] w-full object-cover rounded-xl'
         src='https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' 
@@ -22,7 +28,10 @@ const HeadlineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white '>
             <p className='font-bold text-2xl px-2 pt-4'>Sun's out, BOGO's Out</p>
             <p className='px-2'>Through 9/26</p>
-            <button className='border-white bg-white text-black absolute mx-2 bottom-4'>Order Now</button>
+            <button className='border-white bg-white text-black absolute mx-2 bottom-4 hover:text-white hover:bg-orange-600 
+            hover:scale-105 duration-300'>
+              Order Now
+            </button>
         </div>
         <img className='max-h[160px] md:max-h-[200px] w-full object-cover rounded-xl'
         src='https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' 
@@ -35,7 +44,10 @@ const HeadlineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white '>
             <p className='font-bold text-2xl px-2 pt-4'>Popular in Your Area</p>
             <p className='px-2'>Through 9/26</p>
-            <button className='border-white bg-white text-black absolute mx-2 bottom-4'>Order Now</button>
+            <button className='border-white bg-white text-black absolute mx-2 bottom-4 hover:text-white hover:bg-orange-600 
+            hover:scale-105 duration-300'>
+              Order Now
+            </button>
         </div>
         <img className='max-h[160px] md:max-h-[200px] w-full object-cover rounded-xl'
         src='https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' 
